b46786bc6c960b5cc8b64ad96941b1b5f6697868
HughManPlayer <43358445+HughManPlayer@users.noreply.github.com>
2023-11-15 03:38:17 -0700

    Update PWMTest.hs

--- PWMTest.hs
+++ PWMTest.hs
@@ -1,4 +1,5 @@
 import System.IO
+--import System.Console.ANSI
 
 data TileType = Grass | Water | Bush | Tree | Ash deriving Eq
 
@@ -21,6 +22,8 @@ data Board = Board
   , boardHeight :: Int
   , boardTiles  :: [[Tile]]
   }
+
+--function to update the board with a move
 
 --Function for testing with string representation
 tileToChar :: Tile -> Char
@@ -66,6 +69,10 @@ charToTile 'B' = Tile Bush False 6
 charToTile 'T' = Tile Tree False 9
 charToTile '.' = Tile Ash False 0
 
+--function to clear the console
+--clearConsole :: IO ()
+--clearConsole = clearScreen >> setCursorPosition 0 0
+
 -- Function to read a board from a file
 readBoardFromFile :: FilePath -> IO Board
 readBoardFromFile filePath = do
@@ -76,9 +83,32 @@ readBoardFromFile filePath = do
       boardTiles = map (map charToTile) rows
   return Board { boardWidth = boardWidth, boardHeight = boardHeight, boardTiles = boardTiles }
 
+-- Function to get user input for a move
+getUserMove :: IO (Int, Int)
+getUserMove = do
+  putStrLn "Enter row index (starting from 0):"
+  row <- readLn
+  putStrLn "Enter column index (starting from 0):"
+  col <- readLn
+  return (row, col)
 
+-- Function to update the board by replacing a tile at a given position
+updateTile :: Board -> (Int, Int) -> Tile -> Board
+updateTile board (x, y) newTile =
+  if x >= 0 && x < boardWidth board && y >= 0 && y < boardHeight board
+    then
+      board { boardTiles = updateRow (boardTiles board) y (updateCol (boardTiles board !! y) x newTile) }
+    else
+      board
+  where
+    updateRow rows rowIndex newRow = take rowIndex rows ++ [newRow] ++ drop (rowIndex + 1) rows
+    updateCol cols colIndex newCol = take colIndex cols ++ [newCol] ++ drop (colIndex + 1) cols
 
 main :: Prelude.IO ()
 main = do
-  board <- readBoardFromFile "greenHillZone.txt"
+  putStrLn "Initial board:"
+  board <- readBoardFromFile "bigMapTest.txt"
   printBoard board
+  --clearConsole
+  putStrLn "Updated board:"
+  printBoard (updateTile board (0,0) testerTileBurn)
